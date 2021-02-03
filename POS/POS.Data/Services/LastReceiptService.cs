215d66fa062cb371de29ae4591723141a73ffe66
Anupam <aprakash@diamondgame.com>
2021-02-03 12:41:37 -0500

    Review changes based on pull request( Utilizing high level File system calss)

--- POS/POS.Data/Services/LastReceiptService.cs
+++ POS/POS.Data/Services/LastReceiptService.cs
@@ -3,21 +3,29 @@ using POS.Core.Interfaces;
 using POS.Core.Transaction;
 using System;
 using System.IO;
+using Framework.Core.FileSystem;
 
 namespace POS.Infrastructure.Services
 {
     public class LastReceiptService : ILastReceiptService
     {
         private const string FileName = @"lastreceipt.json";
+        private readonly IFileSystemService _fileSystem;
+        private readonly IFilePathService _filePathService;
 
-    
+        public LastReceiptService(IFileSystemService fileSystem, IFilePathService filePathService)
+        {
+            _fileSystem = fileSystem;
+            _filePathService = filePathService;
+        }
+
         public void SetLastReceipt(int receiptNumber, int voucherCount, decimal totalPayout)
         {
             var last = new LastTransaction(receiptNumber, voucherCount, totalPayout);
 
-            var path = Path.Combine(Environment.CurrentDirectory, FileName);
+            var path = _filePathService.Combine(Environment.CurrentDirectory, FileName);
             // serialize JSON directly to a file
-            using (var file = File.CreateText(path))
+            using (var file = _fileSystem.File.CreateText(path))
             {
                 var serializer = new JsonSerializer();
                 serializer.Serialize(file, last);
@@ -26,10 +34,10 @@ namespace POS.Infrastructure.Services
 
         public LastTransaction GetLastReceipt()
         {
-            var path = Path.Combine(Environment.CurrentDirectory, FileName);
-            if (!File.Exists(path)) return null;
+            var path = _filePathService.Combine(Environment.CurrentDirectory, FileName);
+            if (!_fileSystem.File.Exists(path)) return null;
             // deserialize JSON directly from a file
-            using (StreamReader file = File.OpenText(path))
+            using (var file = _fileSystem.File.OpenText(path))
             {
                 var serializer = new JsonSerializer();
                 return (LastTransaction)serializer.Deserialize(file, typeof(LastTransaction));
