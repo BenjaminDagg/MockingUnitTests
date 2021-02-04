c64d5e3f1abaac04d9cec551016a5ad200827fb6
Anupam <aprakash@diamondgame.com>
2021-02-04 16:08:20 -0500

    Updated Last Receipt service

--- POS/POS.Data/Services/LastReceiptService.cs
+++ POS/POS.Data/Services/LastReceiptService.cs
@@ -30,14 +30,49 @@ namespace POS.Infrastructure.Services
             
           
             var path = _fileSystem.Path.Combine(AppContext.BaseDirectory, FileName);
-
+           
             var receiptObject = _appSettingsFileService.LoadFile(path);
+            if (receiptObject != null )
+            {
+                if (((JProperty) ((JContainer) receiptObject).First).Name == "LastTransaction")
+                {
+                    AddJsonData(receiptObject, path, receiptNumber, voucherCount, totalPayout);
+                }
+                else
+                {
+                    var receiptUpdated= CreateJsonStructure();
+                    AddJsonData(receiptUpdated, path, receiptNumber, voucherCount, totalPayout);
+                }
+                
+            }
+            else
+            {
 
-            _appSettingsFileService.AddOrUpdateJsonObj<int>(receiptObject, $"LastTransaction:LastReceiptNumbers", receiptNumber);
-            _appSettingsFileService.AddOrUpdateJsonObj<int>(receiptObject, $"LastTransaction:LastVoucherCounts", voucherCount);
-            _appSettingsFileService.AddOrUpdateJsonObj<decimal>(receiptObject, $"LastTransaction:LastReceiptTotals", totalPayout);
-            _appSettingsFileService.SaveJsonObjectToFile(receiptObject, path);
-            
+                var newReceiptCreated = CreateJsonStructure();
+                AddJsonData(newReceiptCreated, path, receiptNumber, voucherCount, totalPayout);
+            }
+
+        }
+
+        public void AddJsonData(JObject receipt, string filepath, int receiptNumber, int voucherCount, decimal totalPayout)
+        {
+            _appSettingsFileService.AddOrUpdateJsonObj<int>(receipt, $"LastTransaction:LastReceiptNumbers", receiptNumber);
+            _appSettingsFileService.AddOrUpdateJsonObj<int>(receipt, $"LastTransaction:LastVoucherCounts", voucherCount);
+            _appSettingsFileService.AddOrUpdateJsonObj<decimal>(receipt, $"LastTransaction:LastReceiptTotals", totalPayout);
+            _appSettingsFileService.SaveJsonObjectToFile(receipt, filepath);
+        }
+
+        public JObject CreateJsonStructure()
+        {
+            JObject newReceipt =
+                new JObject(
+                    new JProperty("LastTransaction",
+                        new JObject(
+                            new JProperty("LastReceiptNumbers", 0),
+                            new JProperty("LastVoucherCounts", 0),
+                            new JProperty("LastReceiptTotals", 0)
+                        )));
+            return newReceipt;
         }
 
         public LastTransaction GetLastReceipt()
