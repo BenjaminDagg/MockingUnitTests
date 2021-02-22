a54acd6a07c8f7de430a16d6d4eb447fa758d740
Anupam <aprakash@diamondgame.com>
2021-02-22 10:47:30 -0500

    #50752 Focus shifts automaticly to voucher number text box #50755 Configured Close Button for Kiosk mode

--- POS/POS/Modules/Payout/Views/SearchVoucherView.xaml.cs
+++ POS/POS/Modules/Payout/Views/SearchVoucherView.xaml.cs
@@ -18,15 +18,44 @@ namespace POS.Modules.Payout.Views
     /// </summary>
     public partial class SearchVoucherView : UserControl
     {
+        System.Timers.Timer _timer;
         public SearchVoucherView()
         {
             InitializeComponent();
+           this.Unloaded += SearchVoucherView_Unloaded;
+           this.Loaded += SearchVoucherView_Loaded;
         }
+
+        private void SearchVoucherView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+        }
+
+        private void SearchVoucherView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Elapsed -= _timer_Elapsed;
+            _timer.Stop();
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+               if(VoucherNumberTextBox != null)
+                {
+                    VoucherNumberTextBox.Focus();
+                    VoucherNumberTextBox.CaretIndex = VoucherNumberTextBox.Text.Length;
+               }
+            });
+        }
+
         private void ValidationNumberTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             var t = (TextBox)sender;
             t.Focus();
             t.CaretIndex = t.Text.Length;
-        }
+        }       
     }
 }
