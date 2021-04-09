39e78f8eaa72ef2cde22317bf151992ed5858708
Louis Epstein <lepstein@DiamondGame.com>
2021-04-09 20:31:56 +0000

    Added 8 files to /Deployment/Scripts

---
+++ Deployment/Scripts/Update-AppSettingsThumbprint.ps1
@@ -0,0 +1,7 @@
+﻿param ([string] $jsonFullPath, [String] $thumbprint);
+
+$pathToJson = $jsonFullPath
+$a = Get-Content $pathToJson | ConvertFrom-Json
+$a.ApiConfiguration.SigningCertificateThumbprint = $thumbprint
+
+$a | ConvertTo-Json -Depth 10 | set-content $pathToJson
