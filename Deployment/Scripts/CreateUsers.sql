97abeb78aa8bca57666ac74deacbbf86c47924e6
Louis Epstein <lepstein@DiamondGame.com>
2021-04-12 02:13:12 +0000

    Updated CreateUsers.sql

--- Deployment/Scripts/CreateUsers.sql
+++ Deployment/Scripts/CreateUsers.sql
@@ -1,17 +1,17 @@
 USE [Master]
 
 IF SUSER_ID('mas') IS NULL
-CREATE LOGIN [mas] WITH PASSWORD = 'spoam6us',
+CREATE LOGIN [mas] WITH PASSWORD = '#{UserMasPassword}#',
 CHECK_POLICY = OFF
 
 IF SUSER_ID('sqljobs') IS NULL
-CREATE LOGIN [sqljobs] WITH PASSWORD = 'sq1acc3ss',
+CREATE LOGIN [sqljobs] WITH PASSWORD = '#{UserSqljobsPassword}#',
 CHECK_POLICY = OFF
 
 IF SUSER_ID('reports') IS NULL
-CREATE LOGIN [reports] WITH PASSWORD = 'r3port$',
+CREATE LOGIN [reports] WITH PASSWORD = '#{UserReportsPassword}#',
 CHECK_POLICY = OFF
 
 IF SUSER_ID('tp') IS NULL
-CREATE LOGIN [tp] WITH PASSWORD = 'tp4ssw0rd',
+CREATE LOGIN [tp] WITH PASSWORD = '#{UserTpPassword}#',
 CHECK_POLICY = OFF
