c92d2e4cafd1bc72729bcdc7772a02d4c0537356
Louis Epstein <lepstein@DiamondGame.com>
2021-04-12 17:41:03 +0000

    Added 2 files to /Deployment/Scripts

---
+++ Deployment/Scripts/GiveFullControlFolderPermissions.ps1
@@ -0,0 +1,8 @@
+﻿param ([string] $folderpath, [string] $username );
+
+
+$fullpath = Resolve-Path -Path "$($folderpath)" -Relative
+
+$permissionsvar = """$($username)"":(OI)(CI)F"
+
+& icacls $fullpath /grant $permissionsvar
