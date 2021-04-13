0ec00095e72ee2a7452461a35319c7794fa2c2f6
Louis Epstein <lepstein@DiamondGame.com>
2021-04-13 16:24:51 +0000

    Updated UpdatePromoTicketServerAddress.ps1

--- Deployment/Scripts/UpdatePromoTicketServerAddress.ps1
+++ Deployment/Scripts/UpdatePromoTicketServerAddress.ps1
@@ -1,11 +1,5 @@
 ﻿param ([string] $server, [String] $database , [string]$sqluser, [string]$sqlpass);
 
-$server = "devmolretail01"
-$database = "LotteryRetail"
-$sqluser = "sa"
-$sqlpass = "3m3r@ld!"
-
-
 $currentip = (gwmi win32_networkadapterconfiguration -filter 'ipenabled=true').IpAddress[0]
 
 $sqlQuery = "UPDATE AppConfig SET ConfigValue = '$($currentip)' WHERE ConfigKey = 'ServerAddress'"
