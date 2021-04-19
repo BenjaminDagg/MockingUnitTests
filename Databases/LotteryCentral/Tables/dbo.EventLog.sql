c2de0e327389c453e6b7d83bda2579f09d24afe3
Edris Khestoo <ekhestoo@diamondgame.com>
2021-04-19 10:25:23 -0700

    #56272 Database Cleanup added EventLog, InsertDbInfo and updated filter

--- Databases/LotteryCentral/Tables/dbo.EventLog.sql
+++ Databases/LotteryCentral/Tables/dbo.EventLog.sql
@@ -1,19 +1,15 @@
 CREATE TABLE [dbo].[EventLog]
 (
 [EventLogId] [int] NOT NULL IDENTITY(1, 1),
-[PartionKey] [int] NOT NULL CONSTRAINT [DF_EventLog_PartionKey] DEFAULT (CONVERT([int],replace(CONVERT([char](10),getdate(),(101)),'/',''),(0))),
-[CreatedDate] [datetime] NOT NULL CONSTRAINT [DF_EventLog_EventDate] DEFAULT (getdate()),
+[PartionKey] [int] NOT NULL CONSTRAINT [DF_EventLog_PartionKey] DEFAULT (CONVERT([int],CONVERT([float],getdate(),(0)),(0))),
+[EventDate] [datetime] NOT NULL,
 [EventTypeId] [int] NOT NULL,
-[EventSource] [varchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[EventSourceVersion] [varchar] (64) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[DeviceName] [varchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Message] [varchar] (1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Details] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Description] [varchar] (1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Details] [text] COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[EventSource] [varchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [UserId] [int] NULL,
-[CentralUserId] [int] NULL
+[LocationID] [int] NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[EventLog] ADD CONSTRAINT [PK_EventLog] PRIMARY KEY CLUSTERED  ([EventLogId]) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[EventLog] ADD CONSTRAINT [FK_EventLog_EventType] FOREIGN KEY ([EventTypeId]) REFERENCES [dbo].[EventType] ([EventTypeId])
+ALTER TABLE [dbo].[EventLog] ADD CONSTRAINT [PK_EventLog] PRIMARY KEY CLUSTERED  ([EventLogId], [PartionKey]) ON [PRIMARY]
 GO
